bcde10609b48509831d087f9569ff429ace3e0db
w-xuefeng <xuefeng@kcalb.wang>
2021-11-10 08:27:39 +0000

    chore: add exports for vue and react

---
+++ packages/screen-recorder-react/src/index.ts
@@ -0,0 +1,4 @@
+import ScreenRecorderVue from './components/ScreenRecorder';
+export { default as ScreenRecorderVue } from './components/ScreenRecorder';
+export { default as Video } from './components/Video';
+export default ScreenRecorderVue
